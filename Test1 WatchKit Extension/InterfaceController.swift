75d8267d8fc81b7de7b592455e114b63249e63ad
Scott Clark <clar2242@me.com>
2015-10-08 19:32:00 +0100

    Initial commit from local git

---
+++ Test1 WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Test1 WatchKit Extension
+//
+//  Created by Scott Clark on 08/10/2015.
+//  Copyright © 2015 Scott Clark. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
